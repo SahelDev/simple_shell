53edf17af9cbb9b87df9730748b108f6ffe25ad2
macbook <macbook@macbooks-MacBook-Pro.local>
2023-11-17 03:34:25 +0100

    Update

--- builtin_functions.c
+++ builtin_functions.c
@@ -1,62 +1,66 @@
 #include "shell.h"
 
 /**
- *env - prints the current_environnement
- *@tokenized_command: command entered
+ * print_environment - prints the current environment variables
+ * @tokenized_command: command entered
  *
- *Return: void
+ * This function prints the current environment variables.
+ * Returns: void
  */
-
-void env(char **tokenized_command __attribute__((unused)))
+void print_environment(char **tokenized_command __attribute__((unused)))
 {
-	int i;
+    int env_index;
 
-	for (i = 0; environ[i] != NULL; i++)
-	{
-		print(environ[i], STDOUT_FILENO);
-		print("\n", STDOUT_FILENO);
-	}
+    for (env_index = 0; environ[env_index] != NULL; env_index++)
+    {
+        print(environ[env_index], STDOUT_FILENO);
+        print("\n", STDOUT_FILENO);
+    }
 }
 
 /**
- * quit - exits the shell
+ * shell_exit - exits the shell
  * @tokenized_command: command entered
  *
- * Return: void
+ * This function handles the shell exit command.
+ * Returns: void
  */
-
-void quit(char **tokenized_command)
+void shell_exit(char **tokenized_command)
 {
-	int num_token = 0, arg;
+    int num_tokens = 0, exit_status;
 
-	for (; tokenized_command[num_token] != NULL; num_token++)
-		;
-	if (num_token == 1)
-	{
-		free(tokenized_command);
-		free(line);
-		free(commands);
-		exit(status);
-	}
-	else if (num_token == 2)
-	{
-		arg = _atoi(tokenized_command[1]);
-		if (arg == -1)
-		{
-			print(shell_name, STDERR_FILENO);
-			print(": 1: exit: Illegal number: ", STDERR_FILENO);
-			print(tokenized_command[1], STDERR_FILENO);
-			print("\n", STDERR_FILENO);
-			status = 2;
-		}
-		else
-		{
-			free(line);
-			free(tokenized_command);
-			free(commands);
-			exit(arg);
-		}
-	}
-	else
-		print("$: exit doesn't take more than one argument\n", STDERR_FILENO);
+    for (; tokenized_command[num_tokens] != NULL; num_tokens++)
+        ;
+
+    if (num_tokens == 1)
+    {
+        free(tokenized_command);
+        free(line);
+        free(commands);
+        exit(status);
+    }
+    else if (num_tokens == 2)
+    {
+        exit_status = _atoi(tokenized_command[1]);
+
+        if (exit_status == -1)
+        {
+            print(shell_name, STDERR_FILENO);
+            print(": 1: exit: Illegal number: ", STDERR_FILENO);
+            print(tokenized_command[1], STDERR_FILENO);
+            print("\n", STDERR_FILENO);
+            status = 2;
+        }
+        else
+        {
+            free(line);
+            free(tokenized_command);
+            free(commands);
+            exit(exit_status);
+        }
+    }
+    else
+    {
+        print("$: exit doesn't take more than one argument\n", STDERR_FILENO);
+    }
 }
