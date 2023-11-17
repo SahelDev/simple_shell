342be6fb72bc93880aa66de1719c13cf1366a598
macbook <macbook@macbooks-MacBook-Pro.local>
2023-11-17 03:39:05 +0100

    Update

--- helper_1.c
+++ helper_1.c
@@ -1,121 +1,99 @@
 #include "shell.h"
 
 /**
- *_strcmp - compare two strings
- *@first: first string to be compared
- *@second: second string to be compared
+ * _strcmp - Compares two strings
+ * @first: First string to be compared
+ * @second: Second string to be compared
  *
- * Return: difference of the two strings
+ * Return: Difference between the two strings
  */
+int _strcmp(char *first, char *second) {
+    int index = 0;
 
-int _strcmp(char *first, char *second)
-{
-	int i = 0;
+    while (first[index] != '\0') {
+        if (first[index] != second[index])
+            break;
+        index++;
+    }
 
-	while (first[i] != '\0')
-	{
-		if (first[i] != second[i])
-			break;
-		i++;
-	}
-	return (first[i] - second[i]);
+    return (first[index] - second[index]);
 }
 
 /**
- *_strcat - concatenates two strings
- *@destination: string to be concatenated to
- *@source:  string to concatenate
+ * _strcat - Concatenates two strings
+ * @destination: String to be concatenated to
+ * @source: String to concatenate
  *
- * Return: address of the new string
+ * Return: Address of the new string
  */
+char *_strcat(char *destination, char *source) {
+    char *new_string = NULL;
+    int len_dest = _strlen(destination);
+    int len_source = _strlen(source);
 
-char *_strcat(char *destination, char *source)
-{
-	char *new_string =  NULL;
-	int len_dest = _strlen(destination);
-	int len_source = _strlen(source);
+    new_string = malloc(sizeof(*new_string) * (len_dest + len_source + 1));
+    _strcpy(destination, new_string);
+    _strcpy(source, new_string + len_dest);
+    new_string[len_dest + len_source] = '\0';
 
-	new_string = malloc(sizeof(*new_string) * (len_dest + len_source + 1));
-	_strcpy(destination, new_string);
-	_strcpy(source, new_string + len_dest);
-	new_string[len_dest + len_source] = '\0';
-	return (new_string);
+    return (new_string);
 }
 
 /**
- *_strspn - gets the length of a prefix substring
- *@str1: string to be searched
- *@str2: string to be used
+ * _strspn - Gets the length of a prefix substring
+ * @str1: String to be searched
+ * @str2: String to be used
  *
- *Return: number of bytes in the initial segment of 5 which are part of accept
+ * Return: Number of bytes in the initial segment of 'str1' which are part of 'str2'
  */
+int _strspn(char *str1, char *str2) {
+    int index = 0;
+    int match = 0;
 
-int _strspn(char *str1, char *str2)
-{
-	int i = 0;
-	int match = 0;
+    while (str1[index] != '\0') {
+        if (_strchr(str2, str1[index]) == NULL)
+            break;
+        match++;
+        index++;
+    }
 
-	while (str1[i] != '\0')
-	{
-		if (_strchr(str2, str1[i]) == NULL)
-			break;
-		match++;
-		i++;
-	}
-	return (match);
+    return (match);
 }
 
 /**
- *_strcspn - computes segment of str1 which consists of characters not in str2
- *@str1: string to be searched
- *@str2: string to be used
+ * _strcspn - Computes segment of 'str1' which consists of characters not in 'str2'
+ * @str1: String to be searched
+ * @str2: String to be used
  *
- *Return: index at which a char in str1 exists in str2
+ * Return: Index at which a character in 'str1' exists in 'str2'
  */
+int _strcspn(char *str1, char *str2) {
+    int len = 0;
+    int i;
 
+    for (i = 0; str1[i] != '\0'; i++) {
+        if (_strchr(str2, str1[i]) != NULL)
+            break;
+        len++;
+    }
 
-int _strcspn(char *str1, char *str2)
-{
-	int len = 0, i;
-
-	for (i = 0; str1[i] != '\0'; i++)
-	{
-		if (_strchr(str2, str1[i]) != NULL)
-			break;
-		len++;
-	}
-	return (len);
+    return (len);
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 /**
- *_strchr - locates a char in a string
- *@s: string to be searched
- *@c: char to be checked
+ * _strchr - Locates a character in a string
+ * @s: String to be searched
+ * @c: Character to be checked
  *
- *Return: pointer to the first occurence of c in s
+ * Return: Pointer to the first occurrence of 'c' in 's'
  */
+char *_strchr(char *s, char c) {
+    int i = 0;
 
-char *_strchr(char *s, char c)
-{
-	int i = 0;
-
-	for (; s[i] != c && s[i] != '\0'; i++)
-		;
-	if (s[i] == c)
-		return (s + i);
-	else
-		return (NULL);
+    for (; s[i] != c && s[i] != '\0'; i++)
+        ;
+    if (s[i] == c)
+        return (s + i);
+    else
+        return (NULL);
 }
-
